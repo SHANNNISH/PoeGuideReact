cea2e83193bc27809ee25c3bc67dc0a13053563a
Yaroslav Klishevich <utopia2002@mail.ru>
2024-01-10 01:21:41 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+
+import Header from './components/Header'
+import Basics from './components/Basics'
+import Start from './components/Start'
+import Builds from './components/Builds'
+import Farm from './components/Farm'
+import Atlas from './components/Atlas'
+
+
+
+function App() {
+  return (
+    <>
+      <Header/>
+      <Basics/>
+      <Start/>
+      <Builds/>
+      <Atlas/>
+      <Farm/>
+      
+      
+    </>
+  )
+}
+
+export default App
