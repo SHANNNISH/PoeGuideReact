c74e976b53e76e5bd4ddb7e81ae773f1e1fe6a4f
Yaroslav Klishevich <utopia2002@mail.ru>
2024-01-10 03:57:13 +0300

    s

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/PoeGuideReact/",
   plugins: [react({
-    base: "/PoeGuideReact/",
+    
     include: "*/.jsx",
   })]
 })
